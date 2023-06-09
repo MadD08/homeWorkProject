ae56a294a272d3cf9fa944ff2b50450039470023
dmytro.ivanov <dmytro.ivanov@form.com>
2023-06-09 10:06:41 +0300

    Completed the first home assignment

--- src/homeWorks/MyClass.java
+++ src/homeWorks/MyClass.java
@@ -4,12 +4,11 @@ class MyClass {
     int year = 2023;
 
     public static int sumMethod(int a, int b) {
-        int sum = a + b;
-        return sum;
+        return a + b;
     }
 
     public static void main(String[] args) {
-        System.out.println(sumMethod(1, 2));
+        System.out.println(sumMethod(2, 2));
     }
 }
 
@@ -19,6 +18,8 @@ class MyClass {
  * Also, I took the opportunity and rename the initial method and variable within the method in order
  * for them to clearly state their function.
  * Comment was removed because it stated an obvious fact.
+ * Removed the redundant c variable in order to simplify the code, since there was no need to declare and then return it.
+ * Added sout method in order to visualize actions of our method.
  * Unfortunately, the purpose of the year variable remains unclear to me.
  * However, since I am not the author of the code, I decided to leave it there
  * while keeping in mind that it might be used in the future.*/
