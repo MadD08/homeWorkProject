b2e846e48452128a7dc5d03d235334ea3d1d0a67
dmytro.ivanov <dmytro.ivanov@form.com>
2023-06-27 13:53:22 +0300

    5th HW

--- src/homeWorks/homeWork5/Test.java
+++ src/homeWorks/homeWork5/Test.java
@@ -2,10 +2,10 @@ package homeWorks.homeWork5;
 
 public class Test {
     public static void main(String[] args){
-        Dog Rex = new Dog("Rex");
-        System.out.println(Rex.paw);
+        Dog rex = new Dog("Rex");
+        System.out.println(rex.paw);
 
-        Cat Gandalf = new Cat("Gandalf");
-        Gandalf.sleeps();
+        Cat gandalf = new Cat("Gandalf");
+        gandalf.sleeps();
     }
 }
